6a4a99ca1009b2909499fd0ccb84b02917c62d38
ttolttol2 <ttolttol2@gmail.com>
2020-03-22 09:03:08 +0900

    Create test_midi.py

---
+++ test_midi.py
@@ -0,0 +1,16 @@
+import mido
+
+def read_midi():
+    inport = mido.open_input('mio MIDI 1') #change to your device file
+    while True:
+        try:
+            for msg in inport.iter_pending():
+                if (msg.type == 'note_on'):
+                    print(msg.note, 'on')
+                elif (msg.type == 'note_off'):
+                    print(msg.note, 'off')
+
+        except AttributeError as error:
+            print("Error expected")
+            pass
+read_midi()
