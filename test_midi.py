50894d35e6e3e88223d3b11801b88d946cdebb30
ttolttol2 <ttolttol2@gmail.com>
2020-03-23 21:32:23 +0900

    Update test_midi.py

--- test_midi.py
+++ test_midi.py
@@ -2,16 +2,21 @@
 simple test program that displays on when the key is pressed and off when the key is released. 
 """
 import mido
+import time
+from datetime import timedelta
 
 def read_midi():
     inport = mido.open_input('mio MIDI 1') #change to your device file
     while True:
         try:
+            start = time.time()
             for msg in inport.iter_pending():
-                if (msg.type == 'note_on'):
-                    print(msg.note, 'on')
-                elif (msg.type == 'note_off'):
-                    print(msg.note, 'off')
+                if (msg.type == 'note_on' and msg.velocity > 0):
+                    end = time.time()
+                    print(msg.note, 'on', timedelta(seconds=end-start).microseconds)
+                else:
+                    end = time.time()
+                    print(msg.note, 'off', timedelta(seconds=end-start).microseconds)
 
         except AttributeError as error:
             print("Error expected")
